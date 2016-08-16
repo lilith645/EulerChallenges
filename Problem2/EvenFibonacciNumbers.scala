ed292212ec3eb4bb84e49db75e908f86bfeb72c8
Lilith645 <lilith@inet-sys.com>
2016-08-16 13:59:24 +1000

    Added GPL licence to problem 1 and 2 Changes committed: 	modified:   Problem1/MultiplesOf3and5.scala 	modified:   Problem2/EvenFibonacciNumbers.scala

--- Problem2/EvenFibonacciNumbers.scala
+++ Problem2/EvenFibonacciNumbers.scala
@@ -1,3 +1,23 @@
+/*
+ * EvenFibonacciNumbers.scala
+ * This file is part of EulerChallenges
+ *
+ * Copyright (C) 2016 - Akuma
+ *
+ * EulerChallenges is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * EulerChallenges is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EulerChallenges. If not, see <http://www.gnu.org/licenses/>.
+ */ 
+
 object EvenFibonacciNumbers {
   def isEven(a:Int):Int = if(a%2==0) a else 0
   def sumOfEvenFibonacci(prev1: Int, prev2: Int, limit: Int): Int = {
